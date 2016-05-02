5e24130af41f5d395372b5f4568a2b873bfea1a0
wookie <kononenkoa995@ukr.net>
2016-05-02 22:53:35 +0300

    adding merge sort support

--- src/model/MergeSort.java
+++ src/model/MergeSort.java
@@ -1,6 +1,7 @@
 package model;
 
 /**
+ * Java Merge sort realization.
  * Created by wookie on 5/2/16.
  */
 public class MergeSort implements Sorter {
@@ -11,6 +12,14 @@ public class MergeSort implements Sorter {
         double temp[] = new double[array.length];
         mergeSort(this.array , 0 , array.length , temp);
     }
+
+    /**
+     * Merge sort algorithm realization.
+     * @param array array to sort.
+     * @param start start index.
+     * @param end end index.
+     * @param temp temporary buffer.
+     */
     private void mergeSort(double array[], int start, int end, double temp[]){
         if(start < end - 1) {
             int mid = start + (end - start) / 2;
@@ -20,6 +29,14 @@ public class MergeSort implements Sorter {
         }
     }
 
+    /**
+     * merge two part of one array.
+     * @param array to merge.
+     * @param start start of first part of array.
+     * @param mid middle part of the whole array. End of first part and start of second part.
+     * @param end end of second part of array.
+     * @param resultArray temporary buffer.
+     */
     private void merge(double[] array, int start, int mid, int end, double[] resultArray) {
         int firstIndex = start;
         int secondIndex = mid;
